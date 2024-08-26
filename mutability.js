731dee0a24e4183bc9f1fa4e2c4969d8dc4d1563
moonyango <mosesadrian825@gmail.com>
2024-08-26 15:40:27 +0300

    set get and data structures in js

---
+++ mutability.js
@@ -0,0 +1,11 @@
+let clone1 = {... person}
+let clone2 = Object.assign({}, person)
+
+let samePerson = person
+
+person.age += 1
+person.country = "FR"
+
+if (person !== clone1) {
+    samePerson = person
+}
