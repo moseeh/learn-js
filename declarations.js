6a205f6804475e472e7f6ad78426d9504af71156
moonyango <mosesadrian825@gmail.com>
2024-08-26 13:47:20 +0300

    data types in js

---
+++ declarations.js
@@ -0,0 +1,18 @@
+const escapeStr = "`\\/\"'"
+const arr = [4, "2"]
+const obj = {
+    str: "i am who i am",
+    num: 77,
+    bool: false,
+    undef: undefined
+}
+const nested = {
+    arr: [4, undefined, "2"],
+    obj: {
+        str: "moses is a fool",
+        num: 99,
+        bool: true
+
+    },
+
+}
