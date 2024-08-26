3b4e632028c25a81dc92b013212522dc201e7845
moonyango <mosesadrian825@gmail.com>
2024-08-26 18:13:20 +0300

    signs

--- sign.js
+++ sign.js
@@ -1,11 +1,18 @@
 function sign(num) {
     if (num > 0) {
         return 1
-    }
-    if (num < 0) {
+    } else if (num < 0) {
         return -1
-    }
-    if (num === 0) {
+    } else {
         return 0
     }
+}
+
+function sameSign(num1, num2) {
+    if (sign(num1)=== sign(num2)){
+        return true
+    } else {
+        return false
+    }
+    
 }
