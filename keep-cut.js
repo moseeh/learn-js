1f8ec08179c498863d66f571c55fffec80847d57
moonyango <mosesadrian825@gmail.com>
2024-08-29 18:01:50 +0300

    functions that keep or cut first two or last two elements in a string

--- keep-cut.js
+++ keep-cut.js
@@ -24,7 +24,7 @@ function cutFirstLast(input){
 
 function keepFirst(input) {
     let res = ""
-    for (let i = 0; i < 3; i++) {
+    for (let i = 0; i < 2; i++) {
         res += input[i]
     }
     return res
@@ -43,3 +43,5 @@ function keepLast(input) {
 function keepFirstLast(input) {
     return keepFirst(input) + keepLast(input)
 }
+
+console.log(keepFirst('abcdef'))
