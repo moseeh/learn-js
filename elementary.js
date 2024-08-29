99721b220e0fc5e5385a71be3f327593b3aed588
moonyango <mosesadrian825@gmail.com>
2024-08-29 16:57:31 +0300

    math functions

--- elementary.js
+++ elementary.js
@@ -1,13 +1,26 @@
 function multiply(a,b) {
+    var neg = false 
+    if (a< 0 || b< 0) {
+        neg = true 
+    }
+    if (a<0 && b < 0) {
+        neg = false
+    }
+    a = abs(a)
+    b = abs(b)
+    
     var result = 0
     for(var i = 1; i <= b; i++) {
         result += a
+    }
+    if (neg === true) {
+        result = -result
     }
     return result
 }
 
 function divide(a, b) {
-    if (b === 0) {
+    if (divisor === 0) {
         throw new Error("Division by zero");
     }
 
@@ -35,3 +48,13 @@ function modulus(a, b) {
 
     return multiply(sign , a);
 }
+
+function abs(num) {
+    if (num < 0) {
+        return -num
+    } else {
+        return num 
+    }
+}
+
+console.log(multiply(20,0))
