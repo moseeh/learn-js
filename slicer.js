149a78413d05dcd2130266cf3fb7bf65c52b191a
moonyango <mosesadrian825@gmail.com>
2024-08-29 17:41:48 +0300

    slicing in js

---
+++ slicer.js
@@ -0,0 +1,29 @@
+function slice(input , start, end){
+    if (end === undefined) {
+        end = input.length
+    }
+    if (start < 0) {
+        start = input.length + start
+    }
+    if (end < 0) {
+        end = input.length + end
+    }
+    for (let i = start; i < input.length; i++) {
+
+    }
+    let result
+
+    if (typeof input === "string") {
+        res = ""
+        for (let i = start; i < end; i++) {
+            res += input[i]
+        }
+    }
+    if (Array.isArray(input)) {
+        res = []
+        for (let i = start; i < end; i++) {
+            res.push(input[i])
+        }
+    }
+    return res
+}
