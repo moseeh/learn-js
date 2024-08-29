7a52dcfe91e22b3cd31445ae175762286ca8c7e7
moonyango <mosesadrian825@gmail.com>
2024-08-29 17:01:29 +0300

    math functions

--- elementary.js
+++ elementary.js
@@ -1,30 +1,35 @@
-function multiply(a,b) {
-    var neg = false 
-    if (a< 0 || b< 0) {
-        neg = true 
+function multiply(a, b) {
+    var neg = false;
+    if (a < 0) {
+        a = abs(a);
+        neg = !neg;
     }
-    if (a<0 && b < 0) {
-        neg = false
+    if (b < 0) {
+        b = abs(b);
+        neg = !neg;
     }
-    a = abs(a)
-    b = abs(b)
-    
-    var result = 0
-    for(var i = 1; i <= b; i++) {
-        result += a
+
+    var result = 0;
+    for (var i = 0; i < b; i++) {
+        result += a; // Adding 'a', 'b' times
     }
-    if (neg === true) {
-        result = -result
+
+    if (neg) {
+        result = -result;
     }
-    return result
+
+    return result;
 }
 
 function divide(a, b) {
-    if (divisor === 0) {
+    if (b === 0) {
         throw new Error("Division by zero");
     }
-
+    
     let sign = (a < 0) !== (b < 0) ? -1 : 1;
+
+    a = abs(a);
+    b = abs(b);
 
     let quotient = 0;
     while (a >= b) {
@@ -32,7 +37,7 @@ function divide(a, b) {
         quotient++;
     }
 
-    return multiply(sign,  quotient);
+    return multiply(sign, quotient); // Using 'multiply' to adjust the sign
 }
 
 function modulus(a, b) {
@@ -42,19 +47,22 @@ function modulus(a, b) {
 
     let sign = a < 0 ? -1 : 1;
 
+    a = abs(a);
+    b = abs(b);
+
     while (a >= b) {
         a -= b;
     }
 
-    return multiply(sign , a);
+    return multiply(sign, a); // Returning remainder with correct sign
 }
 
 function abs(num) {
-    if (num < 0) {
-        return -num
-    } else {
-        return num 
-    }
+    return num < 0 ? -num : num;
 }
 
-console.log(multiply(20,0))
+// Testing the divide and modulus functions
+console.log(divide(10, 20));  // Output: 0
+console.log(divide(20, 10));  // Output: 2
+console.log(modulus(20, 10)); // Output: 0
+console.log(modulus(21, 10)); // Output: 1
