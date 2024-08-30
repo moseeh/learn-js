3d97c58c731a1763f0fcec15f2d32d3acb4083ba
moonyango <mosesadrian825@gmail.com>
2024-08-30 13:20:50 +0300

    learn math functions

---
+++ cut-corners.js
@@ -0,0 +1,77 @@
+function round(num) {
+    let neg = false;
+    if (num < 0) {
+        neg = true;
+        num = -num;
+    }
+    
+    let count = 0;
+    while (!(num < 1 && num > -1)) {
+        num -= 1;
+        count++;
+    }
+    
+    if (num < 0.5) {
+        return neg ? -count : count;
+    } else {
+        return neg ? -count - 1 : count + 1;
+    }
+}
+
+function ceil(num) {
+    let neg = false;
+    if (num < 0) {
+        neg = true;
+        num = -num;
+    }
+    
+    let count = 0;
+    while (!(num < 1 && num > -1)) {
+        num -= 1;
+        count++;
+    }
+    
+    if (num > 0) {
+        return neg ? -count - 1 : count + 1;
+    } else {
+        return neg ? -count : count;
+    }
+}
+
+function floor(num) {
+    let neg = false;
+    if (num < 0) {
+        neg = true;
+        num = -num;
+    }
+    
+    let count = 0;
+    while (!(num < 1 && num > -1)) {
+        num -= 1;
+        count++;
+    }
+    
+    return neg ? -count - 1 : count;
+}
+
+function trunc(num) {
+    let neg = false;
+    if (num < 0) {
+        neg = true;
+        num = -num;
+    }
+    
+    let count = 0;
+    while (!(num < 1 && num > -1)) {
+        num -= 1;
+        count++;
+    }
+    
+    return neg ? -count : count;
+}
+
+const nums = [3.7, -3.7, 3.1, -3.1];
+console.log(nums.map(round));
+console.log(nums.map(ceil));
+console.log(nums.map(floor));
+console.log(nums.map(trunc));
