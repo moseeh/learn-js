c510f2a311b21a32d7b417032d5462d5477a61ae
moonyango <mosesadrian825@gmail.com>
2024-09-01 00:47:06 +0300

    compare number of matches

--- same-amount.js
+++ same-amount.js
@@ -1,9 +1,12 @@
-function sameAmount(str, regex1, regex2) {
-    // Get the number of matches for both regular expressions
-    const matches1 = (str.match(regex1) || []).length;
-    const matches2 = (str.match(regex2) || []).length;
-  
-    // Compare the number of matches
-    return matches1 === matches2;
+function sameAmount(str, regExp1, regExp2) {
+    // Ensure the regular expressions have the global flag
+    const re1 = new RegExp(regExp1, 'g');
+    const re2 = new RegExp(regExp2, 'g');
+    
+    // Count the matches for each regular expression
+    const count1 = (str.match(re1) || []).length;
+    const count2 = (str.match(re2) || []).length;
+    
+    // Return true if the counts are the same, false otherwise
+    return count1 === count2;
   }
-  
