f77076c5ede508c61aa025c1a33e2c8ced64dc55
moonyango <mosesadrian825@gmail.com>
2024-09-01 00:43:00 +0300

    compare number of matches

--- same-amount.js
+++ same-amount.js
@@ -1,8 +1,9 @@
 function sameAmount(str, regex1, regex2) {
-    // Get all matches for both regular expressions, defaulting to an empty array if no matches are found
-    const matches1 = str.match(regex1) || [];
-    const matches2 = str.match(regex2) || [];
+    // Get the number of matches for both regular expressions
+    const matches1 = (str.match(regex1) || []).length;
+    const matches2 = (str.match(regex2) || []).length;
   
     // Compare the number of matches
-    return matches1.length === matches2.length;
+    return matches1 === matches2;
   }
+  
