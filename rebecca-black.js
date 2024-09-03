c24138a8e37ccbb628f24d61d713b1406f0477d4
moonyango <mosesadrian825@gmail.com>
2024-09-03 15:39:03 +0300

    isfriday is weekend is leap year and is last day if month

--- rebecca-black.js
+++ rebecca-black.js
@@ -1,33 +1,18 @@
 function isFriday(date) {
-    return new Date(date).getDay() === 5
+    return new Date(date).getDay() === 5;
 }
 
 function isWeekend(date) {
-    return (new Date(date).getDay()=== 0 || new Date(date).getDay() === 6)
+    const day = new Date(date).getDay();
+    return day === 0 || day === 6;
 }
 
 function isLeapYear(date) {
-    return new Date(date).getFullYear() %4 === 0
+    const year = new Date(date).getFullYear();
+    return (year % 4 === 0 && year % 100 !== 0) || (year % 400 === 0);
 }
 
 function isLastDayOfMonth(date) {
-    var day = new Date(date)
-    var year = day.getFullYear()
-    var month = day.getMonth()
-    var d = day.getDate()
-
-    if (month === 1 && isLeapYear(year)&& d === 29) {
-        return true
-    }
-    if (month === 0 || month === 2 || month === 4 || month === 6 || month === 7 || month === 9 || month === 11) {
-        if (d === 31) {
-            return true
-        } 
-    }
-    if (month === 3 || month === 5 || month === 8 || month === 10 ) {
-        if (d === 30) {
-            return true
-        } 
-    }
-    return false 
+    const d = new Date(date);
+    return new Date(d.getFullYear(), d.getMonth() + 1, 0).getDate() === d.getDate();
 }
