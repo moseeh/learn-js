a9a4eff7818cb525cebbb6ed0da81ab6b7096dcb
moonyango <mosesadrian825@gmail.com>
2024-09-04 12:57:12 +0300

    using maps in js

--- using-map.js
+++ using-map.js
@@ -1,52 +1,44 @@
-function citiesOnly(arrobjects) {
-    let arr = []
-    for (let i = 0; i<= arrobjects.length-1; i++) {
-        arr.push(arrobjects[i].city)
-    }
-    return arr
-
+function citiesOnly(arr) {
+    return arr.map((item) => item.city);
 }
 
 function upperCasingStates(arr) {
-    for (let i = 0; i <= arr.length-1; i++) {
-        arr[i] = capitalizeFirstLetterOfEachWord(arr[i])
-    }
-    return arr
-}
-
-function capitalizeFirstLetterOfEachWord(sentence) {
-    return sentence
-      .split(' ')  
-      .map(word => word.charAt(0).toUpperCase() + word.slice(1))  
-      .join(' ')
+    return arr.map((item) =>
+        item
+            .split(" ")
+            .map((word) => {
+                return word[0].toUpperCase() + word.slice(1);
+            })
+            .join(" ")
+    );
 }
 
 function fahrenheitToCelsius(arr) {
-    for (let i = 0; i <= arr.length-1; i++) {
-        arr[i] = fahreToCelsius(arr[i])
-    }
-    return arr
-}
-function fahreToCelsius(fahrenheitStr) {
-    let fahrenheit = parseFloat(fahrenheitStr)
-    let celsius = (fahrenheit - 32) * 5 / 9
-    return celsius.toFixed(0) + "°C"
+    return arr.map(
+        (item) =>
+            Math.floor((Number(item.slice(0, -2)) - 32) * (5 / 9)).toString() +
+            "°C"
+    );
 }
 
-function trimTemp(arrobjects) {
-    for (let i = 0; i< arrobjects.length; i++) {
-        arrobjects[i].temperature = arrobjects[i].temperature.trim()
-    }
-    return arrobjects
+function trimTemp(arr) {
+    return arr.map((item) => {
+        item.temperature = item.temperature.replaceAll(" ", "");
+        return item;
+    });
 }
 
 function tempForecasts(arr) {
     return arr.map((item) => {
-      const celsius = Math.floor((parseFloat(item.temperature) - 32) * (5 / 9));
-      const formattedState = item.state
-        .split(" ")
-        .map((word) => word[0].toUpperCase() + word.slice(1))
-        .join(" ");
-      return `${celsius}°Celsius in ${item.city}, ${formattedState}`;
+        return `${
+            Math.floor(
+                (Number(item.temperature.slice(0, -2)) - 32) * (5 / 9)
+            ).toString() + "°Celsius"
+        } in ${item.city}, ${item.state
+            .split(" ")
+            .map((word) => {
+                return word[0].toUpperCase() + word.slice(1);
+            })
+            .join(" ")}`;
     });
 }
