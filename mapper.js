4977551e674828c7fba62c1ec6277165c3bf3769
moonyango <mosesadrian825@gmail.com>
2024-09-04 15:13:46 +0300

    map and flatmap

---
+++ mapper.js
@@ -0,0 +1,47 @@
+function forEach(array, callback) {
+    if (!Array.isArray(array)) {
+      throw new TypeError('First argument must be an array');
+    }
+    if (typeof callback !== 'function') {
+      throw new TypeError('Second argument must be a function');
+    }
+  
+    for (let i = 0; i < array.length; i++) {
+      callback(array[i], i, array);
+    }
+  }
+  
+  function map(array, callback) {
+    if (!Array.isArray(array)) {
+      throw new TypeError('First argument must be an array');
+    }
+    if (typeof callback !== 'function') {
+      throw new TypeError('Second argument must be a function');
+    }
+  
+    const result = [];
+    for (let i = 0; i < array.length; i++) {
+      result.push(callback(array[i], i, array));
+    }
+    return result;
+  }
+  
+  function flatMap(array, callback) {
+    if (!Array.isArray(array)) {
+      throw new TypeError('First argument must be an array');
+    }
+    if (typeof callback !== 'function') {
+      throw new TypeError('Second argument must be a function');
+    }
+  
+    const result = [];
+    for (let i = 0; i < array.length; i++) {
+      const mappedValue = callback(array[i], i, array);
+      if (Array.isArray(mappedValue)) {
+        result.push(...mappedValue);
+      } else {
+        result.push(mappedValue);
+      }
+    }
+    return result;
+  }
