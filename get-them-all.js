220b1ec2ed4f52ae3d9f904e6c6718082e10942d
moonyango <mosesadrian825@gmail.com>
2024-09-06 16:24:07 +0300

    get bonnano pisano

---
+++ get-them-all.js
@@ -0,0 +1,17 @@
+export function getArchitects() {
+    return [document.querySelectorAll('body a'), document.querySelectorAll('body span')]
+}
+
+export function getClassical() {
+    return [document.querySelectorAll('a.classical'), document.querySelectorAll('a:not(.classical)')]
+}
+
+export function getActive() {
+
+    return [document.querySelector('a.classical.active'), document.querySelectorAll('a.classical:not(.active)')]
+}
+
+export function getBonannoPisano() {
+    return [document.querySelector('#BonannoPisano'),document.querySelectorAll('a.classical.active')]
+
+}
