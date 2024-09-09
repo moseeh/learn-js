5de47aba7726f02adce7ac960d425253306fe623
moonyango <mosesadrian825@gmail.com>
2024-09-09 17:38:25 +0300

    inverted object .. replaces keys with values and values with keys

--- invert.js
+++ invert.js
@@ -1,9 +1,7 @@
 function invert(obj) {
     const invertedobj = {};
     for (const key in obj) {
-        if (obj.hasOwnProperty(key)) {
             invertedobj[obj[key]] = key;
-        }
     }
     return invertedobj;
 }
