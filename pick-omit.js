8132c070a7a3808310e8f4ac587aff2df690ef49
moonyango <mosesadrian825@gmail.com>
2024-09-09 18:03:21 +0300

    pick and omit

--- pick-omit.js
+++ pick-omit.js
@@ -14,7 +14,10 @@ function pomit(obj, str) {
     let newobj = {}
     for (const key in obj) {
         if (obj.hasOwnProperty(key)) {
-            if (!str.includes(key)) {
+            if (Array.isArray(str) &&!str.includes(key)) {
+                newobj[key] = obj[key]
+            }
+            if (typeof str === "string" && str !== key) {
                 newobj[key] = obj[key]
             }
         }
