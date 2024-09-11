d71af8817d0094463b40312625f699a15911b721
moonyango <mosesadrian825@gmail.com>
2024-09-11 15:19:15 +0300

    introduced an initial value in reduce score

--- curry-entries.js
+++ curry-entries.js
@@ -38,17 +38,3 @@ function filterForce(personnel) {
 function mapAverage(personnel) {
   return mapCurry(([k, v]) => [k, { ...v, averageScore: (v.pilotingScore + v.shootingScore) / 2 }])(personnel);
 }
-
-
-const personnel = {
-  lukeSkywalker: { id: 5, pilotingScore: 98, shootingScore: 56, isForceUser: true },
-  sabineWren:    { id: 82, pilotingScore: 73, shootingScore: 99, isForceUser: false },
-  zebOrellios:   { id: 22, pilotingScore: 20, shootingScore: 59, isForceUser: false },
-  ezraBridger:   { id: 15, pilotingScore: 43, shootingScore: 67, isForceUser: true },
-  calebDume:     { id: 11, pilotingScore: 71, shootingScore: 85, isForceUser: true },
-};
-
-
-const result = reduceScore(personnel);
-console.log(result); // Output should be 208
-
