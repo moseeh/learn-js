d7e6f2963c4bc77d46f13c950f43c74faa50a0bb
moonyango <mosesadrian825@gmail.com>
2024-09-14 16:53:12 +0300

    fix: manage timeout

--- debounce.js
+++ debounce.js
@@ -1,4 +1,3 @@
-// Basic debounce function (unchanged)
 function debounce(func, wait) {
   let timeout;
   
@@ -13,19 +12,19 @@ function debounce(func, wait) {
   };
 }
 
-// Updated opDebounce function
+
 function opDebounce(func, wait, options = {}) {
   let timeout;
   let lastCallTime = 0;
-  
+
   return function executedFunction(...args) {
     const currentTime = Date.now();
     const isLeadingCall = options.leading && (currentTime - lastCallTime >= wait);
 
     const later = () => {
       timeout = null;
-      if (!options.leading) func(...args);
       lastCallTime = Date.now();
+      if (!options.leading) func(...args);
     };
 
     if (isLeadingCall) {
@@ -33,8 +32,7 @@ function opDebounce(func, wait, options = {}) {
       func(...args);
     } else {
       clearTimeout(timeout);
+      timeout = setTimeout(later, wait);
     }
-
-    timeout = setTimeout(later, wait);
   };
 }
