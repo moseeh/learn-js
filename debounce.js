3659f1f6d0dd10af05307a2851f1469cc47ebfdf
moonyango <mosesadrian825@gmail.com>
2024-09-14 12:47:43 +0300

    two functions that will work like _.debounce from lodash.

--- debounce.js
+++ debounce.js
@@ -1,40 +1,32 @@
-// Basic debounce function (unchanged)
 function debounce(func, wait) {
   let timeout;
-  
-  return function executedFunction(...args) {
-    const later = () => {
-      clearTimeout(timeout);
-      func(...args);
-    };
-
+  return function (...args) {
+    const context = this;
     clearTimeout(timeout);
-    timeout = setTimeout(later, wait);
+    timeout = setTimeout(() => {
+      func(...args);
+    }, wait);
   };
 }
-
-// Updated opDebounce function
-function opDebounce(func, wait, options = {}) {
-  let timeout;
-  let lastCallTime = 0;
-  
-  return function executedFunction(...args) {
-    const currentTime = Date.now();
-    const isLeadingCall = options.leading && (currentTime - lastCallTime >= wait);
-
-    const later = () => {
-      timeout = null;
-      if (!options.leading) func(...args);
-      lastCallTime = Date.now();
-    };
-
-    if (isLeadingCall) {
-      lastCallTime = currentTime;
-      func(...args);
-    } else {
-      clearTimeout(timeout);
+function opDebounce(fn, delay, options) {
+  var timer = null,
+    first = true,
+    leading;
+  if (typeof options === "object") {
+    leading = !!options.leading;
+  }
+  return function () {
+    let context = this,
+      args = arguments;
+    if (first && leading) {
+      fn.apply(context, args);
+      first = false;
     }
-
-    timeout = setTimeout(later, wait);
+    if (timer) {
+      clearTimeout(timer);
+    }
+    timer = setTimeout(function () {
+      fn.apply(context, args);
+    }, delay);
   };
 }
