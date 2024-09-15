02be85fd13a52ea6d9112f903db474253f8d8b55
moonyango <mosesadrian825@gmail.com>
2024-09-15 04:13:36 +0300

    changed resolve(new Error('timeout')) to reject(new Error('timeout')) To ensure that an actual error is thrown when the timeout occurs

--- keep-trying-or-giveup.js
+++ keep-trying-or-giveup.js
@@ -15,18 +15,11 @@ function retry(count = 3, callback = async () => {}) {
 
 function timeout(delay = 0, callback = async () => {}) {
   return async function (...args) {
-    const timeoutPromise = new Promise((resolve) =>
-      setTimeout(() => resolve(new Error('timeout')), delay)
+    const timeoutPromise = new Promise((_, reject) =>
+      setTimeout(() => reject(new Error('timeout')), delay)
     );
-    const functionCall = new Promise((resolve) =>
-      resolve(callback(...args))
-    );
-    
+    const functionCall = (async () => await callback(...args))();
     const result = await Promise.race([timeoutPromise, functionCall]);
-    
-    if (result instanceof Error) {
-      throw result;
-    }
     return result;
   };
 }
