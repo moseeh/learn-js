4c1f63bac861e8bc76ab4b185bffbfaf95b233ab
moonyango <mosesadrian825@gmail.com>
2024-09-15 05:40:24 +0300

    all but for objects

--- all.js
+++ all.js
@@ -1,10 +1,8 @@
 async function all(objs = {}) {
-  if (Object.keys(objs).length === 0) return {};
-
-  const entries = Object.entries(objs);
-  const resolvedEntries = await Promise.all(
-    entries.map(async ([key, value]) => [key, await value])
-  );
-
-  return Object.fromEntries(resolvedEntries);
+    var res = {};
+    if (Object.keys(objs).length === 0) return {};
+    for (let key in objs) {
+        res[key] = await objs[key];
+    }
+    return res;
 }
