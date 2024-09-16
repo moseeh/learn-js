134f125789ef4c5e786c3102f1876f8a5789d686
moonyango <mosesadrian825@gmail.com>
2024-09-16 17:47:50 +0300

    verydisco but written to a file

---
+++ verydisco-forever.mjs
@@ -0,0 +1,13 @@
+import fs from 'fs/promises'
+
+const arg = process.argv[2]
+let arr = arg.split(' ')
+let arr1 = []
+for (let i = 0; i <= arr.length -1; i++) {
+    let l = arr[i].length
+    let le = Math.round(l/2)
+    arr1.push(arr[i].slice(le) + arr[i].slice(0, le))
+}
+let s = arr1.join(" ")
+
+fs.writeFile('output.txt', s)
