ba83f8886cb725f546deea6faca6324689d72ebe
moonyango <mosesadrian825@gmail.com>
2024-09-16 19:09:46 +0300

    print dir contents

--- tell-me-who.mjs
+++ tell-me-who.mjs
@@ -1,23 +1,21 @@
 import fs from "fs/promises"
 
 let arg = process.argv[2]
-
 if (arg === undefined) {
     arg = process.argv[1]
 }
 
 const data = await fs.readdir(arg, "utf8")
-
 let arr = []
 
-for (let i = 0; i <= data.length -1; i++) {
-    let s = data[i].trimEnd(".json")
+for (let i = 0; i <= data.length - 1; i++) {
+    let s = data[i].replace(/\.json$/, "") 
     let a = s.split("_")
-    arr.push(a[1]+ " " + a[0])
+    arr.push(a[1] + " " + a[0])
 }
 
 arr.sort()
 
-for (let i = 0; i <= arr.length-1; i++) {
-    console.log(String(i+1)+ ". "+ arr[i])
+for (let i = 0; i <= arr.length - 1; i++) {
+    console.log(String(i + 1) + ". " + arr[i])
 }
