4e698f1d3e68d59b2fba21778013e2fecadbccd7
moonyango <mosesadrian825@gmail.com>
2024-09-17 15:05:16 +0300

    encode and decode using base64

---
+++ tell-it-cypher.mjs
@@ -0,0 +1,21 @@
+import fs from 'fs/promises'
+
+let file = process.argv[2]
+let keyword = process.argv[3]
+let filename = process.argv[4]
+const fileData = fs.readFile(file)
+if (keyword === 'encode') {
+    if (filename === undefined) {
+        filename = "cypher.txt"
+    }
+    const base64data = fileData.toString('base64')
+    fs.writeFile(filename, base64data)
+}
+
+if (keyword === 'decode') {
+    if (filename === undefined) {
+        filename = "clear.txt"
+    }
+    const fileData1 = Buffer.from(fileData, 'base64')
+    fs.writeFile(filename, fileData1)
+}
