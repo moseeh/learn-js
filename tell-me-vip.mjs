b4be77b3b1a1a01ac30f4155cd8dca5323a7c2dc
moonyango <mosesadrian825@gmail.com>
2024-09-17 11:42:24 +0300

    promise all implemented

--- tell-me-vip.mjs
+++ tell-me-vip.mjs
@@ -1,29 +1,39 @@
 import fs from "fs/promises";
+import { join } from "path";
 
 let arg = process.argv[2];
 if (arg === undefined) {
   arg = process.argv[1];
 }
 
-const data = await fs.readdir(arg, "utf8");
-let arr = [];
+try {
+  const data = await fs.readdir(arg, "utf8");
+  let arr = [];
 
-for (let i = 0; i <= data.length - 1; i++) {
-  if (saidYes(data[i])) {
-    let s = data[i].replace(/\.json$/, "");
-    let a = s.split("_");
-    arr.push(a[1] + " " + a[0]);
+  for (let i = 0; i <= data.length - 1; i++) {
+    if (await saidYes(join(arg, data[i]))) {  // Use await to handle async function
+      let s = data[i].replace(/\.json$/, "");
+      let a = s.split("_");
+      arr.push(a[1] + " " + a[0]);
+    }
   }
+
+  arr.sort();
+
+  for (let i = 0; i <= arr.length - 1; i++) {
+    console.log(String(i + 1) + ". " + arr[i]);
+  }
+} catch (err) {
+  console.error("Error:", err);
 }
 
-arr.sort();
-
-for (let i = 0; i <= arr.length - 1; i++) {
-  console.log(String(i + 1) + ". " + arr[i]);
-}
-
-function saidYes(filename) {
-    const data = fs.readFile(filename, "utf8")
-    const jsonData = JSON.parse(data)
-    return jsonData.answer === "yes"
+async function saidYes(filename) {
+  try {
+    const data = await fs.readFile(filename, "utf8");  // Await the file read
+    const jsonData = JSON.parse(data);
+    return jsonData.answer === "yes";
+  } catch (err) {
+    console.error("Error reading/parsing file:", filename, err);
+    return false;  // Return false if there's an error
+  }
 }
