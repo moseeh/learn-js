3cdc3b48cc273eec4b3080d66ba4afc6a6e0a102
moonyango <mosesadrian825@gmail.com>
2024-09-17 11:56:15 +0300

    promise all implemented

--- tell-me-vip.mjs
+++ tell-me-vip.mjs
@@ -5,30 +5,40 @@ if (arg === undefined) {
   arg = process.argv[1];
 }
 
-const data = await fs.readdir(arg, "utf8");
-let arr = [];
+try {
+  const data = await fs.readdir(arg, "utf8");
+  let arr = [];
 
-for (let i = 0; i <= data.length - 1; i++) {
-  if (saidYes(data[i])) {
-    let s = data[i].replace(/\.json$/, "");
-    let a = s.split("_");
-    arr.push(a[1] + " " + a[0]);
+  for (let i = 0; i <= data.length - 1; i++) {
+    if (await saidYes(join(arg, data[i]))) {  // Await the async function
+      let s = data[i].replace(/\.json$/, "");
+      let a = s.split("_");
+      arr.push(a[1] + " " + a[0]);
+    }
   }
+
+  arr.sort();
+  let s = "";
+
+  if (arr.length !== 0) {
+    for (let i = 0; i <= arr.length - 1; i++) {
+      s += String(i + 1) + ". " + arr[i] + "\n";
+    }
+  }
+
+  await fs.writeFile("vip.txt", s);  // Await the file write
+
+} catch (err) {
+  console.error("Error:", err);
 }
 
-arr.sort();
-let s = "";
-
-if (arr.length !== 0) {
-  for (let i = 0; i <= arr.length - 1; i++) {
-    s += String(i + 1) + ". " + arr[i] + "\n";
+async function saidYes(filename) {
+  try {
+    const data = await fs.readFile(filename, "utf8");  // Await the file read
+    const jsonData = JSON.parse(data);
+    return jsonData.answer === "yes";
+  } catch (err) {
+    console.error("Error reading/parsing file:", filename, err);
+    return false;
   }
-}
-
-fs.writeFile("vip.txt", s);
-
-function saidYes(filename) {
-  const data = fs.readFile(filename, "utf8");
-  const jsonData = JSON.parse(data);
-  return jsonData.answer === "yes";
 }
