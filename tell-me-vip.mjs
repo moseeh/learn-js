fc9d938a96210c5801f2862033dbf4ca95d6889e
moonyango <mosesadrian825@gmail.com>
2024-09-17 11:39:54 +0300

    promise all implemented

--- tell-me-vip.mjs
+++ tell-me-vip.mjs
@@ -1,68 +1,29 @@
 import fs from "fs/promises";
-import path from "path";
-import { fileURLToPath } from 'url';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+let arg = process.argv[2];
+if (arg === undefined) {
+  arg = process.argv[1];
+}
 
-let arg = process.argv[2] || process.argv[1];
+const data = await fs.readdir(arg, "utf8");
+let arr = [];
 
-async function saidYes(filePath) {
-  try {
-    const data = await fs.readFile(filePath, "utf8");
-    const jsonData = JSON.parse(data);
-    return jsonData.answer === "yes";
-  } catch (error) {
-    return false;
+for (let i = 0; i <= data.length - 1; i++) {
+  if (saidYes(data[i])) {
+    let s = data[i].replace(/\.json$/, "");
+    let a = s.split("_");
+    arr.push(a[1] + " " + a[0]);
   }
 }
 
-async function main() {
-  try {
-    await fs.access(arg);
-    
-    const data = await fs.readdir(arg, "utf8");
-    let arr = [];
+arr.sort();
 
-    for (let file of data) {
-      if (path.extname(file).toLowerCase() === '.json') {
-        const filePath = path.join(arg, file);
-        if (await saidYes(filePath)) {
-          let s = file.replace(/\.json$/, "");
-          let [lastName, firstName] = s.split("_");
-          arr.push(`${firstName} ${lastName}`);
-        }
-      }
-    }
-
-    arr.sort();
-
-    let output = '';
-    arr.forEach((name, index) => {
-      output += `${index + 1}. ${name}\n`;
-    });
-
-    output = output.trim();
-
-    console.log(output);
-    return output;
-
-  } catch (error) {
-    if (error.code === 'ENOENT') {
-      return '';
-    }
-    console.error("An error occurred:", error);
-    throw error;
-  }
+for (let i = 0; i <= arr.length - 1; i++) {
+  console.log(String(i + 1) + ". " + arr[i]);
 }
 
-// Use top-level await instead of IIFE
-if (import.meta.url === `file://${__filename}`) {
-  try {
-    await main();
-  } catch (error) {
-    console.error("An error occurred:", error);
-  }
+function saidYes(filename) {
+    const data = fs.readFile(filename, "utf8")
+    const jsonData = JSON.parse(data)
+    return jsonData.answer === "yes"
 }
-
-export { main };
